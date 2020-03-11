32d714a1abf97bf4a1cbbcb54ac43d6baaca4e25
Anderson Borges <andersonvaleriano.borges@gmail.com>
2020-03-11 01:36:09 -0300

    New URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -53,12 +53,7 @@ class NewVisitor(LiveServerTestCase):
         # Quando ela tecla enter, a página é atualizada, e agora a página lista
         # "1: Buy peacock feathers" como um item em uma lista de tarefas
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
         # item. Ela insere "Use peacock feathers to make a fly" (Usar penas de pavão
@@ -66,12 +61,10 @@ class NewVisitor(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        
 
         # A página é atualizada novamente e agora mostra os dois itens em sua lista
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # Satisfeita, ela volta a dormir
 
